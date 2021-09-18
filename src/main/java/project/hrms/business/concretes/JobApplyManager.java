0cde0611643bf92d410240e94f08c724bbe5757e
enescakr55 <enescakr01@hotmail.com>
2021-09-18 11:56:32 +0300

    11.56 - 18.09.2021

--- src/main/java/project/hrms/business/concretes/JobApplyManager.java
+++ src/main/java/project/hrms/business/concretes/JobApplyManager.java
@@ -2,14 +2,24 @@ package project.hrms.business.concretes;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import project.hrms.business.abstracts.JobApplyService;
 import project.hrms.core.utilities.results.DataResult;
+import project.hrms.dataacess.abstracts.JobApplyDao;
 import project.hrms.entities.concretes.JobApply;
 
 @Service
 public class JobApplyManager implements JobApplyService{
+	
+	JobApplyDao jobApplyDao;
+	
+	@Autowired
+	public JobApplyManager(JobApplyDao jobApplyDao) {
+		super();
+		this.jobApplyDao = jobApplyDao;
+	}
 
 	@Override
 	public DataResult<List<JobApply>> getAll() {
